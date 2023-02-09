35e3eafb7c296d54f9d412884bde7a6d1c8e2e04
samucardonaamaya39 <samucardonaamaya39@gmail.com>
2023-02-09 09:38:02 -0500

    Se agrega el proyecto

---
+++ src/components/pure/greeting.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            age : 29
+
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>
+                    ¡HOLA { this.props.name } !
+                </h1>
+                <h2>
+                    Tu edad es de:{ this.state.age }
+                </h2>
+            </div>
+        );
+    }
+
+    birthday=()=>{
+        
+    }
+
+
+}
+
+
+Greeting.propTypes = {
+    name: PropTypes.string,
+};
+
+
+export default Greeting;
